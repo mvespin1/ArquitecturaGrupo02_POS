661269904a725c78cf23c8778489d8269c6b524a
Juliana Salomé Escobar Campaña <107556745+JulianaEscobar31@users.noreply.github.com>
2025-01-25 13:04:52 -0500

    Update ActualizacionEstadoDTO
    Se añadieron las validaciones en ActualizacionEstadoDTO.

--- src/main/java/ec/edu/espe/pos/controller/dto/ActualizacionEstadoDTO.java
+++ src/main/java/ec/edu/espe/pos/controller/dto/ActualizacionEstadoDTO.java
@@ -1,12 +1,24 @@
 package ec.edu.espe.pos.controller.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ActualizacionEstadoDTO {
     
+    @NotNull(message = "El código único de transacción es obligatorio")
     private String codigoUnicoTransaccion;
+
+    @NotNull(message = "El estado es obligatorio")
+    @Pattern(regexp = "AUT|REC", message = "El estado debe ser AUT (Autorizado) o REC (Rechazado)")
     private String estado;
+
+    @NotNull(message = "El mensaje es obligatorio") 
     private String mensaje;
+
+    @NotNull(message = "El detalle es obligatorio")
     private String detalle;
 } 
