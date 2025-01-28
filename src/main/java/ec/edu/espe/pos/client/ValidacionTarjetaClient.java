ec8916e3d1dbce46ad981021f6f0a16d9d73ed97
mvespin1 <mvespin1@espe.edu.ec>
2025-01-28 00:27:18 -0500

    Update Despliegue
    Update Despliegue

--- src/main/java/ec/edu/espe/pos/client/ValidacionTarjetaClient.java
+++ src/main/java/ec/edu/espe/pos/client/ValidacionTarjetaClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import ec.edu.espe.pos.controller.dto.ValidacionTarjetaDTO;
 
-@FeignClient(name = "validacionTarjeta", url = "http://3.129.23.35")
+@FeignClient(name = "validacionTarjeta", url = "http://18.118.126.105")
 public interface ValidacionTarjetaClient {
 
     @PostMapping("/v1/tarjetas/validar")
